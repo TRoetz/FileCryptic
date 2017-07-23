8113fa137e95564b8b537fe3f909e7301c823ea3
TRoetz <TRoetz@users.noreply.github.com>
2017-07-24 11:08:21 +1200

    Added files.

---
+++ FileCryptic/fmrMain.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace FileCryptic
+{
+   partial class frmMain
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.buttonEncrypt = new System.Windows.Forms.Button();
+         this.statusStripInfo = new System.Windows.Forms.StatusStrip();
+         this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+         this.buttonDecrypt = new System.Windows.Forms.Button();
+         this.labelPassword = new System.Windows.Forms.Label();
+         this.textBoxPassword = new System.Windows.Forms.TextBox();
+         this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+         this.statusStripInfo.SuspendLayout();
+         this.SuspendLayout();
+         // 
+         // buttonEncrypt
+         // 
+         this.buttonEncrypt.Location = new System.Drawing.Point(12, 8);
+         this.buttonEncrypt.Name = "buttonEncrypt";
+         this.buttonEncrypt.Size = new System.Drawing.Size(115, 23);
+         this.buttonEncrypt.TabIndex = 0;
+         this.buttonEncrypt.Text = "Encrypt File";
+         this.buttonEncrypt.UseVisualStyleBackColor = true;
+         this.buttonEncrypt.Click += new System.EventHandler(this.button1_Click);
+         // 
+         // statusStripInfo
+         // 
+         this.statusStripInfo.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+         this.statusStripInfo.Location = new System.Drawing.Point(0, 59);
+         this.statusStripInfo.Name = "statusStripInfo";
+         this.statusStripInfo.Size = new System.Drawing.Size(330, 22);
+         this.statusStripInfo.TabIndex = 1;
+         this.statusStripInfo.Text = "statusStrip1";
+         // 
+         // toolStripStatusLabel
+         // 
+         this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+         this.toolStripStatusLabel.Size = new System.Drawing.Size(39, 17);
+         this.toolStripStatusLabel.Text = "Ready";
+         // 
+         // buttonDecrypt
+         // 
+         this.buttonDecrypt.Location = new System.Drawing.Point(203, 8);
+         this.buttonDecrypt.Name = "buttonDecrypt";
+         this.buttonDecrypt.Size = new System.Drawing.Size(115, 23);
+         this.buttonDecrypt.TabIndex = 2;
+         this.buttonDecrypt.Text = "Decrypt File";
+         this.buttonDecrypt.UseVisualStyleBackColor = true;
+         this.buttonDecrypt.Click += new System.EventHandler(this.button2_Click);
+         // 
+         // labelPassword
+         // 
+         this.labelPassword.AutoSize = true;
+         this.labelPassword.Location = new System.Drawing.Point(13, 38);
+         this.labelPassword.Name = "labelPassword";
+         this.labelPassword.Size = new System.Drawing.Size(56, 13);
+         this.labelPassword.TabIndex = 3;
+         this.labelPassword.Text = "Password:";
+         // 
+         // textBoxPassword
+         // 
+         this.textBoxPassword.Location = new System.Drawing.Point(67, 35);
+         this.textBoxPassword.Name = "textBoxPassword";
+         this.textBoxPassword.Size = new System.Drawing.Size(251, 20);
+         this.textBoxPassword.TabIndex = 4;
+         // 
+         // openFileDialog
+         // 
+         this.openFileDialog.FileName = "openFileDialog";
+         // 
+         // frmMain
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(330, 81);
+         this.Controls.Add(this.textBoxPassword);
+         this.Controls.Add(this.labelPassword);
+         this.Controls.Add(this.buttonDecrypt);
+         this.Controls.Add(this.statusStripInfo);
+         this.Controls.Add(this.buttonEncrypt);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+         this.Name = "frmMain";
+         this.Text = "FileCryptic";
+         this.Load += new System.EventHandler(this.Form1_Load);
+         this.statusStripInfo.ResumeLayout(false);
+         this.statusStripInfo.PerformLayout();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+      private System.Windows.Forms.Button buttonEncrypt;
+      private System.Windows.Forms.StatusStrip statusStripInfo;
+      private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+      private System.Windows.Forms.Button buttonDecrypt;
+      private System.Windows.Forms.Label labelPassword;
+      private System.Windows.Forms.TextBox textBoxPassword;
+      private System.Windows.Forms.OpenFileDialog openFileDialog;
+   }
+}
+
---
+++ FileCryptic/fmrMain.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace FileCryptic
+{
+   public partial class frmMain : Form
+   {
+      public frmMain()
+      {
+         InitializeComponent();
+      }
+
+      private string inFile;
+      private string outFile;
+
+      // Rfc2898DeriveBytes constants:
+      public readonly byte[] salt = new byte[] { 0x03, 0x01, 0x12, 0x60, 0x10, 0x17, 0x50, 0x23 }; // Must be at least eight bytes.  MAKE THIS SALTIER!
+      public const int iterations = 1042; // Recommendation is >= 1000.
+
+      /// <summary>Decrypt a file.</summary>
+      /// <remarks>NB: "Padding is invalid and cannot be removed." is the Universal CryptoServices error.  Make sure the password, salt and iterations are correct before getting nervous.</remarks>
+      /// <param name="sourceFilename">The full path and name of the file to be decrypted.</param>
+      /// <param name="destinationFilename">The full path and name of the file to be output.</param>
+      /// <param name="password">The password for the decryption.</param>
+      /// <param name="salt">The salt to be applied to the password.</param>
+      /// <param name="iterations">The number of iterations Rfc2898DeriveBytes should use before generating the key and initialization vector for the decryption.</param>
+      public void DecryptFile(string sourceFilename, string destinationFilename, string password, byte[] salt, int iterations)
+      {
+         AesManaged aes = new AesManaged();
+         aes.BlockSize = aes.LegalBlockSizes[0].MaxSize;
+         aes.KeySize = aes.LegalKeySizes[0].MaxSize;
+         // NB: Rfc2898DeriveBytes initialization and subsequent calls to   GetBytes   must be eactly the same, including order, on both the encryption and decryption sides.
+         Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, salt, iterations);
+         aes.Key = key.GetBytes(aes.KeySize / 8);
+         aes.IV = key.GetBytes(aes.BlockSize / 8);
+         aes.Mode = CipherMode.CBC;
+         ICryptoTransform transform = aes.CreateDecryptor(aes.Key, aes.IV);
+
+         using (FileStream destination = new FileStream(destinationFilename, FileMode.CreateNew, FileAccess.Write, FileShare.None))
+         {
+            using (CryptoStream cryptoStream = new CryptoStream(destination, transform, CryptoStreamMode.Write))
+            {
+               try
+               {
+                  using (FileStream source = new FileStream(sourceFilename, FileMode.Open, FileAccess.Read, FileShare.Read))
+                  {
+                     source.CopyTo(cryptoStream);
+                  }
+               }
+               catch (CryptographicException exception)
+               {
+                  if (exception.Message == "Padding is invalid and cannot be removed.")
+                     throw new ApplicationException("Universal Microsoft Cryptographic Exception (Not to be believed!)", exception);
+                  else
+                     throw;
+               }
+            }
+         }
+      }
+
+      /// <summary>Encrypt a file.</summary>
+      /// <param name="sourceFilename">The full path and name of the file to be encrypted.</param>
+      /// <param name="destinationFilename">The full path and name of the file to be output.</param>
+      /// <param name="password">The password for the encryption.</param>
+      /// <param name="salt">The salt to be applied to the password.</param>
+      /// <param name="iterations">The number of iterations Rfc2898DeriveBytes should use before generating the key and initialization vector for the decryption.</param>
+      public void EncryptFile(string sourceFilename, string destinationFilename, string password, byte[] salt, int iterations)
+      {
+         AesManaged aes = new AesManaged();
+         aes.BlockSize = aes.LegalBlockSizes[0].MaxSize;
+         aes.KeySize = aes.LegalKeySizes[0].MaxSize;
+         // NB: Rfc2898DeriveBytes initialization and subsequent calls to   GetBytes   must be eactly the same, including order, on both the encryption and decryption sides.
+         Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, salt, iterations);
+         aes.Key = key.GetBytes(aes.KeySize / 8);
+         aes.IV = key.GetBytes(aes.BlockSize / 8);
+         aes.Mode = CipherMode.CBC;
+         ICryptoTransform transform = aes.CreateEncryptor(aes.Key, aes.IV);
+
+         using (FileStream destination = new FileStream(destinationFilename, FileMode.CreateNew, FileAccess.Write, FileShare.None))
+         {
+            using (CryptoStream cryptoStream = new CryptoStream(destination, transform, CryptoStreamMode.Write))
+            {
+               using (FileStream source = new FileStream(sourceFilename, FileMode.Open, FileAccess.Read, FileShare.Read))
+               {
+                  source.CopyTo(cryptoStream);
+               }
+            }
+         }
+      }
+
+      private void Form1_Load(object sender, EventArgs e)
+      {
+         toolStripStatusLabel.Text = "Ready";
+      }
+
+      private void button1_Click(object sender, EventArgs e)
+      {
+         if (String.IsNullOrWhiteSpace(textBoxPassword.Text))
+         {
+            MessageBox.Show("Please provide a password.", "Password Required", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+         }
+
+         if (openFileDialog.ShowDialog() == DialogResult.OK)
+         {
+            inFile = openFileDialog.FileName;
+
+            if (File.Exists(inFile))
+            {
+               outFile = inFile + ".cryptic";
+               try
+               {
+                  EncryptFile(inFile, outFile, textBoxPassword.Text, salt, iterations);
+               }
+               catch(Exception ex)
+               {
+                  MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                  return;
+               }
+               toolStripStatusLabel.Text = "File: " + inFile + " Encrypted";
+            }
+         }
+      }
+
+      private void button2_Click(object sender, EventArgs e)
+      {
+         openFileDialog.Filter = "cryptic files (*.cryptic)|*.cryptic|All files (*.*)|*.*";
+         openFileDialog.FilterIndex = 2;
+         openFileDialog.RestoreDirectory = true;
+         openFileDialog.Multiselect = false;
+
+         if (String.IsNullOrWhiteSpace(textBoxPassword.Text))
+         {
+            MessageBox.Show("Please provide a password.", "Password Required", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+         }
+
+         if (openFileDialog.ShowDialog() == DialogResult.OK)
+         {
+            inFile = openFileDialog.FileName;
+
+            if (File.Exists(inFile))
+            {
+               outFile = inFile.Replace(".cryptic","");
+               try
+               {
+                  DecryptFile(inFile, outFile, textBoxPassword.Text, salt, iterations);
+               }
+               catch (Exception ex)
+               {
+                  MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                  return;
+               }
+               toolStripStatusLabel.Text = "File: " + outFile + " Decrypted";
+            }
+         }
+      }
+
+   }
+}
